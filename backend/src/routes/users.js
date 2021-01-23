b5e82d1882d45116bf66c323d541f902d1f711be
lirialro <lirialro@gmail.com>
2021-01-23 22:58:47 +0100

    actualiza

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,23 @@
 const {Router} = require('express');
 const router = Router();
+const User = require('../models/User');
 
-router.get('/api/users', (req, res) => {
-    res.json('User list');
+const faker = require('faker');
+
+router.get('/api/users', async (req, res) => {
+    const users = await User.find();
+    res.json({users});
+});
+
+router.get('/api/users/create', async (req, res) => {
+    for (let i = 0; i < 5; i++) {
+        await User.create({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            avatar: faker.image.avatar()
+        });
+    }
+    res.json({message: '5 Users created'});
 });
 
 module.exports = router;
